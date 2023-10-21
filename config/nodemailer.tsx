7ea841c5c78d749e8be732d88383f5862ad5c9e0
Robert Castillo <roca1388@gmail.com>
2023-10-21 16:40:54 -0300

    update commit

--- config/nodemailer.tsx
+++ config/nodemailer.tsx
@@ -2,15 +2,19 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
+const clientId = process.env.OAUTH2_CLIENT_ID;
+const clientSecret = process.env.OAUTH2_CLIENT_SECRET;
 
 export const transporter = nodemailer.createTransport({
 
   host:"https://silvana-iota.vercel.app/",
-  secure:false,
+  secure:true,
   service: "Gmail",
   auth: {
     user: email,
     pass: pass,
+    clientId: clientId,
+    clientSecret: clientSecret,
   },
 });
 
